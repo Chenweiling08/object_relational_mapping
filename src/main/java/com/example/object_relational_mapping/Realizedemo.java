77be102d132b8c7b0a57e4464c34c89bff1a398c
13424 <1342451304@qq.com>
2022-05-17 17:15:49 +0800

    新增查询2张表

--- src/main/java/com/example/object_relational_mapping/Realizedemo.java
+++ src/main/java/com/example/object_relational_mapping/Realizedemo.java
@@ -2,6 +2,8 @@ package com.example.object_relational_mapping;
 
 import com.example.object_relational_mapping.po.Demo;
 import com.example.object_relational_mapping.po.DomainDataObject;
+import com.example.object_relational_mapping.po.Schedule;
+import jdk.nashorn.internal.objects.annotations.Where;
 import lombok.SneakyThrows;
 import org.springframework.stereotype.Service;
 
@@ -18,14 +20,18 @@ import java.util.List;
 public class Realizedemo implements Demo {
     @PersistenceContext
     private EntityManager em;
+    List<Integer> TableNum=new ArrayList<>();
     List<String> TableName = new ArrayList<>();
+    List<String> TableWhere=new ArrayList<>();
 
     @SneakyThrows
     @Override
     public Boolean addTable(Class s) {
         DomainDataObject a=(DomainDataObject)s.getAnnotation(DomainDataObject.class);
         String value = a.value();
+       int num=a.num();
         TableName.add(value);
+        TableNum.add(num);
         return true;
     }
     @Override
@@ -34,18 +40,16 @@ public class Realizedemo implements Demo {
         String judge = s.getSimpleName();
         // sql
         for (int i = 0; i < TableName.size(); i++) {
-//            String sql = "select * from " + TableName.get(i) + "";
             StringBuilder sql = new StringBuilder(" select *  ");
             sql.append(" from " + TableName.get(i) + "");
-            if (judge.equals(TableName.get(i))) {
+            if (judge.equals(TableName.get(i))||TableNum.get(i)>1) {
                 // 执行查询 并把结果专为实体类
-                Query query = em.createNativeQuery(sql.toString(), s);
+                Query query = em.createNativeQuery(sql.toString(),s);
                 // 获取查询结果
                 result = query.getResultList();
-
-
             }
         }
+
         return result;
     }
 }
---
+++ src/main/java/com/example/object_relational_mapping/vo/Result.java
@@ -0,0 +1,30 @@
+package com.example.object_relational_mapping.vo;
+
+import com.example.object_relational_mapping.po.DomainDataObject;
+import lombok.Data;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import java.sql.Time;
+
+/**
+ * @Author:陈炜灵
+ * @Date:2022/5/13 11:16
+ * @Version 1.0
+ */
+@Data
+@Entity
+@DomainDataObject(value = "Schedule,Course",num =2)
+public class Result {
+    @Id
+    private String schId;
+    private String couNumber;
+    private Time schTime;
+    private String Teacher;
+    private String Classroom;
+    private String couId;
+    private  String stuClass;
+    private  String couName;
+    private Time couTime;
+}
