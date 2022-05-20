0000e0a0f65101ce6ea3c5815d654704f2cef889
swj369451 <swj369451@163.com>
2022-05-20 09:04:20 +0800

    添加

---
+++ src/test/java/com/example/object_relational_mapping/TestJpaSpecificationExecutor.java
@@ -0,0 +1,30 @@
+package com.example.object_relational_mapping;
+
+/**
+ * @program: object_relational_mapping
+ * @description
+ * @author: 苏敏
+ * @create: 2022-05-20 09:00
+ **/
+public class TestJpaSpecificationExecutor {
+
+    //学生PO，姓名，性别，年龄
+    //课程PO，课程名，学分，
+    //选课表PO，学生id，课程id，成绩
+
+    //查询1张表
+
+    //两张表查询
+
+    //多张表
+
+    //选择特定的字段
+
+    //where语句
+
+    //orderby语句
+
+    //groupby语句
+
+
+}
